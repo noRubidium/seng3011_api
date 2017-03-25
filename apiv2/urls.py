ace17f4f884982f1d2978cbb33ab26a9a7160456
Minjie Shen <mshen@cse.unsw.edu.au>
2017-03-25 17:32:40 +1100

    Add the url routing and the mock for testing

--- apiv2/urls.py
+++ apiv2/urls.py
@@ -1,9 +1,16 @@
 from django.conf.urls import url
 
+from .mock import mock_views
 from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    # Add proper routing to the view
-    # doc should be in Django website
+    url(r'^merch/([^/]+)/([^/]+)$', views.showMerchandiseData, name="merch"),
+    url(r'^merch/([^/]+)$', views.showMerchandiseData, name="merch"),
+    url(r'^retail/([^/]+)/([^/]+)$', views.showRetailData, name="retail"),
+    url(r'^retail/([^/]+)', views.showRetailData, name="retail"),
+    url(r'^mock/merch/([^/]+)/([^/]+)$', mock_views.showMerchandiseData, name="merch"),
+    url(r'^mock/merch/([^/]+)$', mock_views.showMerchandiseData, name="merch"),
+    url(r'^mock/retail/([^/]+)/([^/]+)$', mock_views.showRetailData, name="retail"),
+    url(r'^mock/retail/([^/]+)', mock_views.showRetailData, name="retail"),
 ]
