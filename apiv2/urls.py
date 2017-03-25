8e7dcd1c48cee71f65bb4c1440dc81c0bfc70726
Minjie Shen <mshen@cse.unsw.edu.au>
2017-03-25 17:50:00 +1100

    Change mock testing

--- apiv2/urls.py
+++ apiv2/urls.py
@@ -3,14 +3,19 @@ from django.conf.urls import url
 from .mock import mock_views
 from . import views
 
+merchUrl = r'merch/([^/]+)$'
+merchUrlLong = r'merch/([^/]+)/([^/]+)$'
+retailUrlLong = r'retail/([^/]+)/([^/]+)$'
+retailUrl = r'retail/([^/]+)$'
+
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^merch/([^/]+)/([^/]+)$', views.showMerchandiseData, name="merch"),
-    url(r'^merch/([^/]+)$', views.showMerchandiseData, name="merch"),
-    url(r'^retail/([^/]+)/([^/]+)$', views.showRetailData, name="retail"),
-    url(r'^retail/([^/]+)', views.showRetailData, name="retail"),
-    url(r'^mock/merch/([^/]+)/([^/]+)$', mock_views.showMerchandiseData, name="merch"),
-    url(r'^mock/merch/([^/]+)$', mock_views.showMerchandiseData, name="merch"),
-    url(r'^mock/retail/([^/]+)/([^/]+)$', mock_views.showRetailData, name="retail"),
-    url(r'^mock/retail/([^/]+)', mock_views.showRetailData, name="retail"),
+    url(r'^' + merchUrl, views.showMerchandiseData, name="merch"),
+    url(r'^' + merchUrlLong, views.showMerchandiseData, name="merch"),
+    url(r'^' + retailUrl, views.showRetailData, name="retail"),
+    url(r'^' + retailUrlLong, views.showRetailData, name="retail"),
+    url(r'^mock/' + merchUrlLong, mock_views.showMerchandiseData, name="merch"),
+    url(r'^mock/' + merchUrl, mock_views.showMerchandiseData, name="merch"),
+    url(r'^mock/' + retailUrlLong, mock_views.showRetailData, name="retail"),
+    url(r'^mock/' + retailUrl, mock_views.showRetailData, name="retail"),
 ]
