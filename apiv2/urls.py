dd3ab90b7929fc96aab90f0151672cbe59a25c2e
Minjie Shen <mshen@cse.unsw.edu.au>
2017-03-25 18:24:38 +1100

    Add 404 not found

--- apiv2/urls.py
+++ apiv2/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from django.http import HttpResponse
 
 from .mock import mock_views
 from . import views
@@ -22,5 +23,9 @@ def makeRoutingWithMock(name, urlbase):
         return pairMaking("showRetailData")
     return []
 
+def notFound(request):
+    return HttpResponse("Please do not try to brute force traverse all the url!", status=404)
+
 urlpatterns = [url(r'^$', views.index, name='index')]  + makeRoutingWithMock(MERCH, shortUrl) \
-              + makeRoutingWithMock(MERCH, longUrl) + makeRoutingWithMock(RETAIL, shortUrl) + makeRoutingWithMock(RETAIL, longUrl)
+              + makeRoutingWithMock(MERCH, longUrl) + makeRoutingWithMock(RETAIL, shortUrl) + makeRoutingWithMock(RETAIL, longUrl)\
+              + [url(r'.', notFound)]
