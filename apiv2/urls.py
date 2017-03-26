2ca9740b6d7c8624c4219396a94f0409599778f7
PranavAB <pranav@Pranavs-MBP.gateway>
2017-03-26 23:01:48 +1100

    added routing for Retail, printing debugging info

--- apiv2/urls.py
+++ apiv2/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^Retail/([a-zA-Z0-9,]*)/([a-zA-Z0-9,]*)$', views.showRetailData),
     # Add proper routing to the view
     # doc should be in Django website
 ]
--- apiv2/views.py
+++ apiv2/views.py
@@ -3,16 +3,20 @@ from django.http import HttpResponse
 # django.json or something...
 
 def index(request):
-    return HttpResponse("This is the API end point.")
+    return HttpResponse("This is the API end point v2.")
 
 
 def showMerchandiseData(request, categories, states="AUS"):
+    return HttpResponse("This is the API end point v2.")
     # get start/end date from request
     # init a Merchandise Object
     # put JSON dump of that
     pass  # return JSON response
 
 def showRetailData(request, categories, states="AUS"):
+    startDate = request.GET.get('startDate', 'defaultStartDate')
+    endDate = request.GET.get('endDate', 'defaultEndDate')
+    return HttpResponse("categories: " + categories + " <br>states: " + states + " <br>startDate: " + startDate + " <br>endDate: " + endDate)
     # get start/end date from request
     # init a Merchandise Object
     # put JSON dump of that
