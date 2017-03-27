ea8f95352162ec36dcb4839d061f7dd1607270fc
PranavAB <pranav.b96@gmail.com>
2017-03-27 13:38:06 +1100

    moved to utils

--- apiv2/utils.py
+++ apiv2/utils.py
@@ -1,3 +1,53 @@
 # A lookup table thingy here ( states/category -> number)
 def lookup(something):
     return something
+
+
+def convertToAbs(string,lookup):
+    splitCategories = [x.strip() for x in string.split(',')]
+    convertedString = ""
+    for category in splitCategories:
+        convertedString += lookup(str(category)) + "+"
+    return convertedString[:-1]
+
+def getCategoryNumber(category):
+    return {
+        'Total': '20',
+        'Food': '41',
+        'HouseholdGood': '42',
+        'ClothingFootwareAndPersonalAccessory': '43',
+        'DepartmentStores': '44',
+        'CafesResturantsAndTakeawayFood': '46',
+        'Other': '45'
+    }.get(category, 'NA')
+
+def getStateNumber(state):
+    return {
+        'Total': '-',
+        'NoStateDetails': '9',
+        'ReExports': 'F',
+        'AUS': '0',
+        'NSW': '1',
+        'WA': '5',
+        'SA': '4',
+        'ACT': '8',
+        'VIC': '2',
+        'TAS': '6',
+        'QLD': '3',
+        'NT': '7'
+    }.get(state, 'NA')
+
+def getCommodityNumber(category):
+    return {
+        'Total': '-1',
+        'FoodAndLiveAnimals': '0',
+        'BeveragesAndTobacco': '1',
+        'CrudMaterialAndInedible': '2',
+        'MineralFuelLubricentAndRelatedMaterial': '3',
+        'AnimalAndVegitableOilFatAndWaxes': '4',
+        'ChemicalsAndRelatedProducts': '5',
+        'ManufacutedGoods': '6',
+        'MachineryAndTransportEquipments': '7',
+        'OtheranucacturedArticles': '8',
+        'Unclassified': '9'
+    }.get(category, 'NA')
--- apiv2/views.py
+++ apiv2/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from utils import getCommodityNumber,getCategoryNumber,getStateNumber,convertToAbs
 
 # django.json or something...
 
@@ -36,53 +37,3 @@ def showRetailData(request, categories, states="AUS"):
     # init a Merchandise Object
     # put JSON dump of that
     pass  # return JSON response
-
-
-def convertToAbs(string,lookup):
-    splitCategories = [x.strip() for x in string.split(',')]
-    convertedString = ""
-    for category in splitCategories:
-        convertedString += lookup(str(category)) + "+"
-    return convertedString[:-1]
-
-def getCategoryNumber(category):
-    return {
-        'Total': '20',
-        'Food': '41',
-        'HouseholdGood': '42',
-        'ClothingFootwareAndPersonalAccessory': '43',
-        'DepartmentStores': '44',
-        'CafesResturantsAndTakeawayFood': '46',
-        'Other': '45'
-    }.get(category, 'NA')
-
-def getStateNumber(state):
-    return {
-        'Total': '-',
-        'NoStateDetails': '9',
-        'ReExports': 'F',
-        'AUS': '0',
-        'NSW': '1',
-        'WA': '5',
-        'SA': '4',
-        'ACT': '8',
-        'VIC': '2',
-        'TAS': '6',
-        'QLD': '3',
-        'NT': '7'
-    }.get(state, 'NA')
-
-def getCommodityNumber(category):
-    return {
-        'Total': '-1',
-        'FoodAndLiveAnimals': '0',
-        'BeveragesAndTobacco': '1',
-        'CrudMaterialAndInedible': '2',
-        'MineralFuelLubricentAndRelatedMaterial': '3',
-        'AnimalAndVegitableOilFatAndWaxes': '4',
-        'ChemicalsAndRelatedProducts': '5',
-        'ManufacutedGoods': '6',
-        'MachineryAndTransportEquipments': '7',
-        'OtheranucacturedArticles': '8',
-        'Unclassified': '9'
-    }.get(category, 'NA')
