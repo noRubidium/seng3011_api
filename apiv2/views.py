fff2d80c2267e8fdd93ca251341d0bf2bc1c64c5
Minjie Shen <mshen@cse.unsw.edu.au>
2017-03-29 17:59:47 +1100

    update hi

--- apiv2/views.py
+++ apiv2/views.py
@@ -1,5 +1,6 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from utils import get_commodity_number,get_category_number,get_state_number
+from parse import parse_merchandise, parse_retail
 
 # django.json or something...
 
@@ -14,8 +15,8 @@ def showMerchandiseData(request, commodities, states="AUS"):
     # init a Merchandise Object (pass in the args needed, look at models.py)
     # get the JSON file with the get_data method or something like that
 
-    # result = parse_merchandise(jsonfile)
-    # return JsonResponse(result)
+    # result = parse_merchandise("{}")
+    return JsonResponse({})
 
 def showRetailData(request, categories, states="AUS"):
     startDate = request.GET.get('startDate', None)
@@ -24,5 +25,5 @@ def showRetailData(request, categories, states="AUS"):
     # init a Retail Object (pass in the args needed, look at models.py)
     # get the JSON file with the get_data method or something like that
 
-    # result = parse_commodity(jsonfile)
-    # return JsonResponse(result)
+    # result = parse_retail("{}")
+    return JsonResponse({})
