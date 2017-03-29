556cb52ea8243ebe600981938cb040acd58a16d0
Minjie Shen <mshen@cse.unsw.edu.au>
2017-03-29 16:33:41 +1100

    Rename function to snake case

--- apiv2/urls.py
+++ apiv2/urls.py
@@ -7,25 +7,31 @@ from . import views
 MERCH = "merch"
 RETAIL = "retail"
 
-shortUrl = r'{0}/(?P<categories>[^/]+)/$'
-longUrl = r'{0}/(?P<categories>[^/]+)/(?P<states>[^/]+)/?$'
+shortUrl = r'{0}/(?P<categories>[A-Za-z]+)/?$'
+longUrl = r'{0}/(?P<categories>[A-Za-z]+)/(?P<states>[A-Za-z]+)/?$'
 
-def makeRoutingWithMock(name, urlbase):
-    def makeRouting(uri, fromModule, methodName):
-        return url(uri, getattr(fromModule, methodName))
-    def pairMaking(methodName):
-        return [makeRouting(r'^' + urlbase.format(name), views, methodName),
-                makeRouting(r'^mock/' + urlbase.format(name), mock_views, methodName)]
+
+def make_routing_with_mock(name, urlbase):
+    def make_routing(uri, from_module, method_name):
+        return url(uri, getattr(from_module, method_name))
+
+    def pair_making(method_name):
+        return [make_routing(r'^' + urlbase.format(name), views, method_name),
+                make_routing(r'^mock/' + urlbase.format(name), mock_views, method_name)]
 
     if name == MERCH:
-        return pairMaking("showMerchandiseData")
+        return pair_making("showMerchandiseData")
     if name == RETAIL:
-        return pairMaking("showRetailData")
+        return pair_making("showRetailData")
     return []
 
-def notFound(request):
+
+def not_found(request):
     return HttpResponse("Please do not try to brute force traverse all the url!", status=404)
 
-urlpatterns = [url(r'^$', views.index, name='index')]  + makeRoutingWithMock(MERCH, shortUrl) \
-              + makeRoutingWithMock(MERCH, longUrl) + makeRoutingWithMock(RETAIL, shortUrl) + makeRoutingWithMock(RETAIL, longUrl)\
-              + [url(r'.', notFound)]
+urlpatterns = [url(r'^$', views.index, name='index')] \
+              + make_routing_with_mock(MERCH, shortUrl) \
+              + make_routing_with_mock(MERCH, longUrl) \
+              + make_routing_with_mock(RETAIL, shortUrl) \
+              + make_routing_with_mock(RETAIL, longUrl)\
+              + [url(r'.', not_found)]
