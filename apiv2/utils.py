d4870734674483d84bca471774346d5135b4b56b
vaishnavi-bapat <vaishnavi.bapat@gmail.com>
2017-03-30 10:55:42 +1100

    changed aus mapping

--- apiv2/utils.py
+++ apiv2/utils.py
@@ -65,7 +65,7 @@ def get_state_abbrev(state):
         '-': 'Total',
         '9': 'NoStateDetails',
         'F': 'ReExports',
-        '0': 'AUS',
+        'Whole Australia': 'AUS',
         'New South Wales': 'NSW',
         'Western Australia': 'WA',
         'South Australia': 'SA',
