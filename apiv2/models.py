7c3fafccab0aa33c32738a5c757a8670cc3bb720
Minjie Shen <mshen@cse.unsw.edu.au>
2017-03-30 23:28:54 +1100

    move state to show more meaningful error message

--- apiv2/models.py
+++ apiv2/models.py
@@ -8,10 +8,7 @@ import re, urllib, urllib2, json
 # JSON can be implemented by using JSON library (https://docs.python.org/2/library/json.html)
 class RemoteResponse:
     def __init__(self, type, categories, states, starting_date, ending_date):
-        
-        # translate the human readable thing to 0.1 0.2 thing (ABS query)
-        # note we are using get_state_number directly.. not using utils.lookup
-        state_numbers = map(get_state_number, states)
+
         
         # convert 'YYYY-MM-DD' to 'YYYY-MM'
         start_month = re.sub(r'(\d{4}-\d{2})-\d{2}',r'\1', starting_date)
@@ -23,23 +20,28 @@ class RemoteResponse:
         values = {'startTime': start_month, 'endTime': end_month, 'dimensionAtObservation': 'allDimensions'}
         data = urllib.urlencode(values)
         url = 'http://stat.data.abs.gov.au/sdmx-json/data/'
-
+        plus = "+"
         if type == 'merch':
             category_numbers = map(get_commodity_number, categories)
-            plus = "+"
-            states_string = plus.join(state_numbers)
             categories_string = plus.join(category_numbers)
+
+            # translate the human readable thing to 0.1 0.2 thing (ABS query)
+            # note we are using get_state_number directly.. not using utils.lookup
+            state_numbers = map(get_state_number, states)
+            states_string = plus.join(state_numbers)
 
             # URL Format: url/MERCH_EXP/{states-numbers}.{categories-numbers}.{industry-of-origin}.{country-of-dest}.M(monthly data)/all
             url += 'MERCH_EXP/' + states_string + "." + categories_string + ".-1.-.M/all"
 
         elif type == 'retail':
             category_numbers = map(get_category_number, categories)
-
-            plus = "+"
-            states_string = plus.join(state_numbers)
             categories_string = plus.join(category_numbers)
 
+
+            # translate the human readable thing to 0.1 0.2 thing (ABS query)
+            # note we are using get_state_number directly.. not using utils.lookup
+            state_numbers = map(get_state_number, states)
+            states_string = plus.join(state_numbers)
             # URL Format: url/RT/{states-numbers}.{data-type}.{categories-numbers}.{adjustment-type(original/seasonal/trend)}.M(monthly data)/all
             url += 'RT/' + states_string + ".2." + categories_string + ".10.M/all"
 
