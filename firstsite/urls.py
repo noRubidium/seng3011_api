371578ef4afe421f961e5197979a9d4511b5bcfa
Minjie Shen <markshen5295@hotmail.com>
2017-04-12 21:53:23 +1000

    Revert "v3"

--- firstsite/urls.py
+++ firstsite/urls.py
@@ -21,5 +21,4 @@ urlpatterns = [
     url(r'^v1/', include('apiv1.urls')),
     url(r'^v1.1/', include('apiv1-1.urls')),
     url(r'^v2/', include('apiv2.urls')),
-    url(r'^v3/', include('apiv3.urls')),
 ]
