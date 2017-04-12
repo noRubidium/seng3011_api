43bf1bbbf0da86c92d653a7510810f66a8844f67
PranavAB <pranav.b96@gmail.com>
2017-04-12 15:29:34 +1000

    merge conflict fix maybe

--- tests/test1_spec.js
+++ tests/test1_spec.js
@@ -1,8 +1,6 @@
 var frisby = require('frisby')
 var data = require('./test_data/test_1_output.json')
 var URL_VERSION = process.env.URL_VERSION
-
-
 frisby.create('RT/Food Method')
           .get(URL_VERSION + 'Retail/Food/')
           .expectStatus(200)
