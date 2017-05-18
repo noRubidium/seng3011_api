dfb59dc39dd282889ab60dc55740be605fbce3c7
Clinton Hadinata <csghadinata@gmail.com>
2017-05-19 06:52:23 +1000

    Added download/parse/nlp on article. Some reformatting.

--- news/views.py
+++ news/views.py
@@ -2,11 +2,13 @@
 The view layer of the API, handle string beautify and stuff
 """
 import json
-from django.http import HttpResponse, JsonResponse
+from django.http import JsonResponse
 from .crocs import cross_origin
 
 import urllib2
+import re
 from newspaper import Article
+from bs4 import BeautifulSoup
 
 @cross_origin
 def get_company_news(request, company):
@@ -18,28 +20,31 @@ def get_company_news(request, company):
     """
 
     news = {}
-
     news_urls = []
 
-    from bs4 import BeautifulSoup
     url = 'http://www.afr.com/research-tools/{}/share-prices/shares-news'.format(company)
 
     conn = urllib2.urlopen(url)
     html = conn.read()
 
     soup = BeautifulSoup(html)
-    links = soup.find_all('a', href=re.compile('^http://www.afr.com/Page/Uuid/*')).get('href',None)
+    links = soup.find_all('a', href=re.compile('^http://www.afr.com/Page/Uuid/*'))
 
-    for link in links:
+    for tag in links:
+        link = tag.get('href',None)
     	if link is not None:
             news_urls.append(link)
 
     counter = 0
-
     for news_url in news_urls:
         a = Article(news_url)
+        a.download()
+        a.parse()
+        a.nlp()
         news_dict = {'headline': a.title, 'date': a.publish_date, 'summary': a.summary, 'image': a.top_image}
         news[str(counter)] =  news_dict
         counter += 1
+
+    # print news
 
     return JsonResponse(news)
